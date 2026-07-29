22d474bbb97a1f4eb1f8670cb713c7136daa6722
weslorde <wsm_10@hotmail.com>
2026-07-29 15:55:26 -0300

    -Cards da galeria indicando icone das 3 skills

--- lib/auxiliar/heroi_class.dart
+++ lib/auxiliar/heroi_class.dart
@@ -1,11 +1,46 @@
 class Heroi {
   final int indice;
   final String nome;
+  final String skillVermelha;
+  final String skillVerde;
+  final List<String> classe;
 
-  const Heroi({required this.indice, required this.nome});
+  const Heroi({
+    required this.indice,
+    required this.nome,
+    required this.skillVermelha,
+    required this.skillVerde,
+    required this.classe,
+  });
 }
 
 const List<Heroi> herois = [
-  Heroi(indice: 0, nome: 'Alton'),
-  Heroi(indice: 1, nome: 'Jorge'),
+  Heroi(
+    indice: 0,
+    nome: 'Alton',
+    skillVermelha: 'Repete',
+    skillVerde: 'InstAgil',
+    classe: ['Suporte'],
+  ),
+  Heroi(
+    indice: 1,
+    nome: 'Jorge',
+    skillVermelha: 'Rebola',
+    skillVerde: 'NuvemCura',
+    classe: ['Suporte', 'Dano'],
+  ),
+  Heroi(
+    indice: 2,
+    nome: 'Alton',
+    skillVermelha: 'Repete',
+    skillVerde: 'InstAgil',
+    classe: ['Suporte'],
+  ),
+  Heroi(
+    indice: 3,
+    nome: 'Jorge',
+    skillVermelha: 'Rebola',
+    skillVerde: 'NuvemCura',
+    classe: ['Suporte', 'Dano'],
+  ),
 ];
--- lib/tdherois_page.dart
+++ lib/tdherois_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:wdungeon/auxiliar/heroi_class.dart';
 import 'package:wdungeon/detalheheroi_page.dart';
@@ -14,7 +16,7 @@ class TdHerois extends StatelessWidget {
         itemCount: herois.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 0.85,
+          childAspectRatio: 0.8,
         ),
         itemBuilder: (context, index) {
           final heroi = herois[index];
@@ -25,18 +27,68 @@ class TdHerois extends StatelessWidget {
                 MaterialPageRoute(builder: (_) => DetalheHeroi(heroi: heroi)),
               );
             },
-            child: Column(
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      'images/perfil/${heroi.nome}.png',
-                      fit: BoxFit.fitHeight,
-                    ),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  child: Stack(
+                    alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Positioned.fill(
+                        child: Image.asset(
+                          'images/perfil/${heroi.nome}.png',
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      //
+                      //
+                      //
+                      Container(
+                        height: 60,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              child: Container(
+                                color: const Color.fromARGB(110, 244, 67, 54),
+                                child: Center(
+                                  child: Image.asset(
+                                    'images/icons/${heroi.skillVermelha}.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                color: const Color.fromARGB(110, 11, 208, 63),
+                                child: Center(
+                                  child: Image.asset(
+                                    'images/icons/${heroi.skillVerde}.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                color: const Color.fromARGB(110, 33, 149, 243),
+                                child: Center(
+                                  child: Image.asset(
+                                    'images/icons/${heroi.skillVerde}.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
           );
         },
